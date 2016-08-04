2f0d964198e799a28a58c3659a499558d58f22de
Scott Conrad <scott@honopu.com>
2016-08-04 09:57:02 -0400

    one more test
    TODO: handle click event and look for 239 for time left.. well actually gameLength - 1;

--- test/button_test.js
+++ test/button_test.js
@@ -1,10 +1,13 @@
 
 import React from 'react';
 import {mount, shallow} from 'enzyme';
+import sinon from 'sinon';
 import Button from './../src/components/Button';
 import state from './../src/state/App';
 const wrapper = shallow(<Button />);
 const mounted = mount(<Button />);
+
+const firstNode = mounted.nodes[0];
 
 mounted.setProps({data:state,in:0});
 describe('<Button />', () => {
@@ -18,18 +21,18 @@ describe('<Button />', () => {
   });
 
   it('should return 0 wins',()=>{
-    let wins = mounted.nodes[0].getWins();
+    let wins = firstNode.getWins();
     expect(wins).to.eql(0);
   });
 
-  it('should return 240 seconds for game length', ()=> {
-    //this is actually a great test since I sometimes reset the game length to 3 for manual testing
+  it('should return 240 seconds for game length from state', ()=> {
     expect(state.gameLength).to.eql(240);
   });
 
-
-
-
-
+  it('should return 240 seconds for game length from component', ()=> {
+    //this is actually a great test since I sometimes reset the game length to 3 for manual testing
+    let gameLength = firstNode.getCountdown();
+    expect(gameLength).to.eql(240);
+  });
 
 });
