d2879fdbf4435febc18625c9bcd3280e0a32cdc4
Scott Conrad <scott@honopu.com>
2016-08-04 08:53:22 -0400

    few more tests

--- test/.setup.js
+++ test/.setup.js
@@ -1,6 +1,12 @@
 require('babel-register')();
 import {expect} from 'chai';
 import sinon from 'sinon';
-
+import jsdom from 'jsdom'
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.document = doc;
+global.window = doc.defaultView;
+global.navigator = {
+  userAgent: 'node.js'
+};
 global.expect = expect;
 global.sinon = sinon;
--- test/button_test.js
+++ test/button_test.js
@@ -2,17 +2,31 @@
 import React from 'react';
 import {mount, shallow} from 'enzyme';
 import Button from './../src/components/Button';
+import state from './../src/state/App';
+const wrapper = shallow(<Button />);
+const mounted = mount(<Button />);
 
+mounted.setProps({data:state,in:0});
 describe('<Button />', () => {
 
   it('calls componentDidMount', () => {
     expect(1).to.equal(1);
   });
 
-  it('should have a div warpper',() => {
-    const wrapper = shallow(<Button />);
-    const mounted = mount(<Button />);
-    console.log(mounted);
+  it('should have a div wrapper',() => {
     expect(wrapper.type()).to.eql('div');
   });
+
+  it('should return 0 wins',()=>{
+    let wins = mounted.nodes[0].getWins();
+    expect(wins).to.eql(0);
+  });
+
+  it('should return 240 seconds for game length', ()=> {
+    expect(state.gameLength).to.eql(240);
+  });
+
+
+
+
 });
